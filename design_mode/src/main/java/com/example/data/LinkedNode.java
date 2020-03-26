f555402bd34741efd1ff86c2220af83648df6641
guhongliang <guhongliang@xiaozhu.com>
2020-03-26 14:28:32 +0800

    add all linked node about

--- design_mode/src/main/java/com/example/data/LinkedNode.java
+++ design_mode/src/main/java/com/example/data/LinkedNode.java
@@ -12,27 +12,30 @@ import java.util.Stack;
  */
 public class LinkedNode {
     //头结点
-    Node head = null;
+    private Node head = null;
 
-    static int[] a = {1, 2, 5, 6, 8, 9};
-    static int[] b = {0, 3, 4, 7, 10};
+    private static int[] a = {1, 2, 5, 6, 8, 9};
+    private static int[] b = {0, 3, 4, 7, 10};
+    private static int[] c = {1, 2, 3, 4, 7, 9};
 
     public static void main(String[] args) {
         LinkedNode linkedNode = createLinkedNode();
-        LinkedNode node1 = createLinkedNode();
-        reversedLinkedList(linkedNode);
+//        LinkedNode node1 = createLinkedNode();
+//        reversedLinkedList(linkedNode);
 //        printReversedList(linkedNode.head);
 //        reversedLinkedHead(linkedNode.head);
-        System.out.println("\n递归反转：\n");
-        log(reversedNodeR(linkedNode.head));
-
-        midNode(node1.head);
-        findKNode(node1.head, 2);
+//        System.out.println("\n递归反转：\n");
+//        log(reversedNodeR(linkedNode.head));
+//
+//        midNode(node1.head);
+//        findKNode(node1.head, 2);
 
 //        sortNode(linkedNode.head);
-//        Node node = mergeNode(createNode(new Node(0), a), createNode(new Node(0), b));
+//        Node node = mergeNode(createNode(a), createNode(b));
 //        log(node);
 //        log(linkedNode.head);
+
+        findSameSortNode(createNode(a), createNode(c));
     }
 
     private static LinkedNode createLinkedNode() {
@@ -56,23 +59,23 @@ public class LinkedNode {
         return linkedNode;
     }
 
-    private static Node createNode(Node head, int[] datas) {
-        Node cur = head;
-        for (int data : datas) {
-            cur.next = new Node(data);
-            cur = cur.next;
+    private static Node createNode(int[] datas) {
+        Node cur = new Node(datas[0]);
+        Node temp = cur;
+        for (int i = 1; i < datas.length; i++) {
+            temp.next = new Node(datas[i]);
+            temp = temp.next;
         }
 
-        log(head);
-        return head;
+        log(cur);
+        System.out.println("\n");
+        return cur;
     }
 
     /**
      * 求链表的长度
-     *
-     * @return
      */
-    public int length() {
+    private static int length(Node head) {
         int length = 0;
         Node curNode = head;
         while (curNode != null) {
@@ -105,8 +108,8 @@ public class LinkedNode {
      * 链表删除结点(1.指定位置的)
      * 将要删除的结点的前结点指向要删除的结点的后结点，
      */
-    public boolean deleteNode(int index) {
-        if (index < 1 || index > length()) {//结点不存在
+    public boolean deleteNode(Node head, int index) {
+        if (index < 1 || index > length(head)) {//结点不存在
             return false;
         }
         if (index == 1) {//删除头结点
@@ -154,7 +157,7 @@ public class LinkedNode {
      * 判断两个链表是否相交
      * 两个链表相交，则它们的尾结点一定是相同的,1、同时遍历两个链表到结尾，进行比较 2、使用栈  类似于1，只不过是根据栈顶元素来判断是否相同，原理相同 3、暴力，嵌套循环遍历
      */
-    public static boolean isCross1(Node head1, Node head2) {
+    private static boolean isCross1(Node head1, Node head2) {
         Node temp1 = head1;
         Node temp2 = head2;
 
@@ -181,19 +184,19 @@ public class LinkedNode {
     }
 
     /**
-     * 一个链表是否带环
+     * 一个链表是否带环  并返回快慢指针的相遇点
      */
-    public static boolean isCross3(Node head) {
+    private static Node isCross3(Node head) {
         Node fast = head, slow = head;
 
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
             if (slow == fast) {
-                return true;
+                return fast;
             }
         }
-        return false;
+        return null;
     }
 
     /**
@@ -226,18 +229,21 @@ public class LinkedNode {
     /**
      * 链表的入口
      * <p>
-     * 通过我们第一个利用开两个指针来判断链表是否带环的原理列出来的，我们得到的结果是 x = k * len - y,表示从开始到入口的结点个数等于 k 倍的环的长度减去入口到相遇结点的个数，那么我们用 cur 指向开始，cur 和 meet 同步向后遍历，当 cur == meet 时，两个指针所在位置即入口。
+     * 通过我们第一个利用开两个指针来判断链表是否带环的原理列出来的，我们得到的结果是 x = k * len - y,
+     * 表示从开始到入口的结点个数等于 k 倍的环的长度减去入口到相遇结点的个数，那么我们用 cur 指向开始，
+     * cur 和 meet 同步向后遍历，当 cur == meet 时，两个指针所在位置即入口。
      */
 
-    private static void findEntranceNode(Node node, Node meet) {
+    private static Node findEntranceNode(Node node, Node meet) {
         Node cur = node;
         if (node == null) {
-            return;
+            return null;
         }
         while (cur != meet) {
             cur = cur.next;
             meet = meet.next;
         }
+        return cur;
     }
 
 
@@ -258,18 +264,18 @@ public class LinkedNode {
 
 
     /**
-     * 求两个链表相交的第一个节点
+     * 求两个链表相交的第一个节点（不带环结构）
      * 1、先判断两个链表是否相交
      * 2、求出两个链表的长度之差 len=length1-length2
      * 3、让长的链表先走len步
      * 4、然后两个链表同步向前移动，每次移动就比较节点是否相等，第一个相等的节点即为第一个相交点
      */
 
-    public Node firstCrossPoint(LinkedNode list1, LinkedNode list2) {
+    public static Node firstCrossPoint(LinkedNode list1, LinkedNode list2) {
         if (!isCross1(list1.head, list2.head))
             return null;
-        int len1 = list1.length();
-        int len2 = list2.length();
+        int len1 = length(list1.head);
+        int len2 = length(list2.head);
 
         int len = len1 - len2;
 
@@ -292,6 +298,72 @@ public class LinkedNode {
         }
 
         return temp1;
+    }
+
+    /**
+     * 判断两个链表是否相交，若相交，求交点。（假设链表可能带环）
+     *
+     * @param first  第一个链表
+     * @param second 第二个链表
+     *               https://blog.csdn.net/bryant_xw/article/details/89761339
+     */
+    private static void firstCircleCrossPoint(Node first, Node second) {
+        // 先验证是否有环  并返回各自的meet点
+        Node p1 = isCross3(first);
+        Node p2 = isCross3(second);
+
+        //如果一个有，一个没有，则肯定不会相交
+        if (p1 == null || p2 == null) {
+            System.out.println("两个链表不会相交");
+            return;
+        }
+
+        //分别寻找两个链表的入口处
+        Node entrance1 = findEntranceNode(first, p1);
+        Node entrance2 = findEntranceNode(first, p2);
+
+        Node cur1 = first;
+        Node cur2 = second;
+        int len1 = 0;
+        int len2 = 0;
+        int gaps;
+
+        //如果入口相同，则
+        if (p1 == p2) {
+            //分别获取两个链表的长度
+            while (cur1 != entrance1) {
+                len1++;
+                cur1 = cur1.next;
+            }
+
+            while (cur2 != entrance2) {
+                len2++;
+                cur2 = cur2.next;
+            }
+
+            //求出两个链表的差值
+            gaps = Math.abs(len1 - len2);
+            cur1 = first;
+            cur2 = second;
+
+            if (len1 < len2) {
+                cur1 = second;
+                cur2 = first;
+            }
+
+            //长度长的链表先走gaps步
+
+            while (gaps-- >= 0) {
+                cur1 = cur1.next;
+            }
+            while ((cur1 != cur2)) {
+                cur1 = cur1.next;
+                cur2 = cur2.next;
+            }
+
+            System.out.println("相遇点为：" + cur1.data);
+        }
+        System.out.println("相遇点有两个分别为为：" + entrance1.data + entrance2.data);
     }
 
     /**
@@ -532,6 +604,33 @@ public class LinkedNode {
         }
 
         System.out.println("倒数第k个结点为：" + slow.data);
+    }
+
+
+    /**
+     * 求两个已排序单链表中相同的数据
+     * <p>
+     * 既然两个链表是有序的 ，那么只需要各自从前往后比较遍历，每次把最小值打印出来。
+     */
+
+    private static void findSameSortNode(Node n1, Node n2) {
+        if (n1 == null || n2 == null) {
+            return;
+        }
+
+        Node cur1 = n1, cur2 = n2;
+        System.out.println("相同的数据为");
+        while (cur1 != null && cur2 != null) {
+            if (cur1.data < cur2.data) {
+                cur1 = cur1.next;
+            } else if (cur1.data > cur2.data) {
+                cur2 = cur2.next;
+            } else {
+                System.out.print(cur2.data + "\t");
+                cur1 = cur1.next;
+                cur2 = cur2.next;
+            }
+        }
     }
 
 }
