c08c72767860de2036e0e49133673d74330078fc
guhongliang <guhongliang@xiaozhu.com>
2020-05-11 19:25:39 +0800

    add reflect class and parameter type

---
+++ design_mode/src/main/java/com/example/base/java/reflect/MyInvoke.java
@@ -0,0 +1,5 @@
+package com.example.base.java.reflect;
+
+public class MyInvoke {
+
+}
---
+++ design_mode/src/main/java/com/example/base/java/reflect/ReflectClassMain.java
@@ -0,0 +1,14 @@
+package com.example.base.java.reflect;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReflectClassMain extends Params<MyClass, MyInvoke> {
+    public static void main(String[] args) {
+//        ReflectClassMain reflectClassMain =
+        new ReflectClassMain();
+
+        List<String> list = new ArrayList<>();
+        list.add("你好啊");
+    }
+}
