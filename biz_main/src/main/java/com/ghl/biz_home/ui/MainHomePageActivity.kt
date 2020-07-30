dacac36505cb36f1341417c926c8665f0cad14e9
guhongliang <guhongliang@xiaozhu.com>
2020-07-30 18:08:51 +0800

    PD：首页改造

--- biz_main/src/main/java/com/ghl/biz_home/ui/MainHomePageActivity.kt
+++ biz_main/src/main/java/com/ghl/biz_home/ui/MainHomePageActivity.kt
@@ -13,6 +13,10 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 @Route(ACTIVITY_HOME)
 class MainHomePageActivity : AppCompatActivity() {
 
+    private val mNavigator: BottomNavigationView by lazy {
+        findViewById<BottomNavigationView>(R.id.nav_view)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_home_page)
@@ -28,5 +32,42 @@ class MainHomePageActivity : AppCompatActivity() {
         //R.id.navigation_home, R.id.navigation_flutter, R.id.navigation_notifications, R.id.navigation_mine))
         //setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+//        mNavigator.run{
+//            setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+//            selectedItemId = R.id.navigation_home
+//        }
+
+
+
+
     }
+
+
+//    /**
+//     * NavigationItemSelect监听
+//     */
+//    private val onNavigationItemSelectedListener =
+//            BottomNavigationView.OnNavigationItemSelectedListener { item ->
+//                setFragment(item.itemId)
+//                return@OnNavigationItemSelectedListener when (item.itemId) {
+//                    R.id.navigation_home -> {
+//                        if (currentIndex == R.id.navigation_home) {
+//                            homeFragment?.smoothScrollToPosition()
+//                        }
+//                        currentIndex = R.id.navigation_home
+//                        true
+//                    }
+//                    R.id.navigation_type -> {
+//                        if (currentIndex == R.id.navigation_type) {
+//                            typeFragment?.smoothScrollToPosition()
+//                        }
+//                        currentIndex = R.id.navigation_type
+//                        true
+//                    }
+//                    else -> {
+//                        false
+//                    }
+//                }
+//            }
 }
---
+++ biz_main/src/main/java/com/ghl/biz_home/ui/activity/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.ghl.biz_home.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ghl.lib_dirty.constants.main.ACTIVITY_HOME
+import com.ghl.router.lib.Router
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Router.with(this).target(ACTIVITY_HOME).start()
+        finish()
+    }
+}
