7f7f2b4fcd2b58f07c81485ce2cdac268457cb14
guhongliang <guhongliang@xiaozhu.com>
2020-08-12 13:35:52 +0800

    PD：新增上拉加载下拉刷新widget

---
+++ flutter_mall/lib/widgets/refresh/pull_to_refresh.dart
@@ -0,0 +1,15 @@
+/*
+    Author: Jpeng
+    Email: peng8350@gmail.com
+    createTime:2018-05-03 15:39
+ */
+
+library pulltorefresh;
+
+export 'smart_refresher.dart';
+export 'indicator/classic_indicator.dart';
+export 'indicator/waterdrop_header.dart';
+export 'indicator/custom_indicator.dart';
+export 'internals/refresh_physics.dart';
+export "internals/indicator_wrap.dart";
+export 'internals/refresh_localizations.dart';
